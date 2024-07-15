6cc6583a5b073b6346e7c5d3be44443a57e5903d
Lekaa29 <orvol905@gmail.com>
2024-07-16 00:06:39 +0200

    Add project files.

---
+++ Ticket Manager/Auction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Manager
+{
+    internal class Auction
+    {
+        
+        private Stadium venue;
+        private Match match;
+        private int price;
+        private string stand;
+        private List<string> seats;
+        
+
+        public Auction(Stadium venue, Match match, string stand, List<string> seats, int price)
+        {
+            this.seats = seats;
+            this.venue = venue;
+            this.match = match;
+            this.price = price;
+            this.stand = stand;
+        }
+
+        public int GetAvailable() { return  seats.Count; }
+        public int Price {  get { return price; } }
+        public Stadium Stadium { get { return this.venue; } }
+        public Match Match { get { return this.match; } }
+        public string Stand { get { return this.stand; } }
+        public string GetSeat () 
+        {
+            string seat = seats[0];
+            seats.RemoveAt(0);
+            return seat;
+        }
+
+        public void AddSeat(string seat)
+        {
+            seats.Add(seat);
+        }
+
+    }
+}
---
+++ Ticket Manager/Match.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Manager
+{
+    internal class Match
+    {
+        private string teamHome;
+        private string teamAway;
+        private DateTime date;
+        private Competition competition;
+
+        private Dictionary<int, int> prices;
+
+        public Match(string teamHome, string teamAway, DateTime date, Competition competition)
+        {
+            this.teamHome = teamHome;
+            this.teamAway = teamAway;
+            this.date = date;
+            this.competition = competition;
+        }
+        public string TeamHome { get {  return teamHome; } }
+        public string TeamAway { get {  return teamAway; } }
+        public DateTime Date { get { return date; } }
+        public Competition Competition { get {  return competition; } }
+    }
+}
